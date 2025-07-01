afc7499652850516435a7bc38265238a645eba4f
core i land <core i land@DESKTOP-7S6DOCF>
2025-07-01 16:43:56 +0330

    extra files deleted.

--- Lab.Infrastructure.Log/DocumentTypeManager.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab.Infrastructure.OperationLog;
-
-public static class DocumentTypeManager
-{
-    private static readonly Dictionary<int, string> Dictionary = new()
-    {
-        { DocumentTypeStore.EquipmentName, "نام تجهیزات" },
-    };
-}
--- Lab.Infrastructure.Log/DocumentTypeStore.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Lab.Infrastructure.OperationLog;
-
-public static class DocumentTypeStore
-{
-    public const int EquipmentName = 1;
-}
